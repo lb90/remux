6915cd346b6275ce112d9704a72e9e267fa02560
Luca Bacci <luca.bacci982@gmail.com>
2018-01-30 19:09:40 +0100

    initial conevrt impl

--- src/mediaconvert.cpp
+++ src/mediaconvert.cpp
@@ -2,230 +2,128 @@
 #include <cassert>
 #include <vector>
 #include <string>
+#include <stdexcept>
 #include <functional>
-#include <glib.h>
-#include <gio/gio.h>
-#include <boost/algorithm/string.hpp>
+#include "app.h"
 #include "model.h"
 #include "launchprocess.h"
 #include "mediaconvert.h"
 
-mediaconvert::convert_context_t::convert_context_t(media_t& elem)
- : elem(elem),
-   outfile_ac3(),
-   outfile_aac(),
-   ac3itaidx()
-   producedv()
-{
-	outfile_ac3 = util_build_filename(settings::work_dir, "temp.ac3");
-	outfile_aac = util_build_filename(settings::work_dir, "temp.aac");
-	
-	if (elem.options.ac3ita_aac) {
-		ac3ita_tid = util_get_ac3ita_track(elem);
-		if (ac3ita_tid.empty())
-			elem.options.ac3ita_aac = false;
-	}
-}
+std::string util_plain_comma_list(const std::vector<size_t>& indexv) {
+	std::string commasequence;
 
-gboolean util_callable_call(gpointer userdata) {
-	callable_t *callable = (callable_t*) userdata;
+	if (indexv.empty())
+		throw std::logic_error("empty track-id list");
 	
-	(*callable)();
-	
-	delete callable;
-}
-
-void mediaconvert::convert(std::function) {
-	std::vector<size_t> indexv;
-
-	for (size_t i = 0; i < elementv.size(); i++)
-		if (elementv[i].ready)
-			indexv.push_back(i);
-	
-	if (indexv.empty()) {
-		util_message("Non sono presenti media in coda");
-		return;
+	commasequence = std::to_string(indexv[0]);
+	for (size_t i = 1; i < indexv.size(); i++) {
+		commasequence += "," + std::to_string(indexv[i]);
 	}
 	
-	for (size_t i : indexv) {
-		std::string outstring;
-		convert(i, outstring);
-		g_idle_add(util_callable_call, );
-	}
+	return 
 }
 
-int mediaconvert::set_attributes(std::string& outstring) {
-	int code;
-
-	std::vector<std::string> edit_argv;
-	edit_argv.emplace_back("mkvpropedit");
-	edit_argv.emplace_back("-e");
-	edit_argv.emplace_back("track:=" + uid);
-	edit_argv.emplace_back("-s");
-	edit_argv.emplace_back("flag-default=1");
-	edit_argv.emplace_back("-s");
-	edit_argv.emplace_back("flag-forced=1");
-	
-	code = launch_process(edit_argv, outstring);
-	if (code != 0)
-		return -1;
-	
-	return 0;
-}
-
-void mediaconvert::select_wanted_audio_tracks(std::vector<int>& want_tid) {
-	for (const auto& item : cvtctx.elem.itemv) {
-		if (item.itemtype == ITEMTYPE_AUDIO) {
-			switch (item.codec_id) {
-				case CODEC_ID_AC3:
-					if (item.opt.intact_ac3)
-						want_tid.push_back(item.tid);
-					break;
-				case CODEC_ID_DOLBY:
-					if (item.opt.intact_dts)
-						want_tid.push_back(item.tid);
-					break;
-				default:
-					break;
-			}
-		}
-	}
-}
-
-int util_get_first_video_track(media_t& elem) {
-	for (const item_t& item : elem.itemv)
-		if (item.itemtype == ITEMTYPE_VIDEO)
-			return item.tid;
-
-	return -1;
-}
-
-int mediaconvert::remove_add_tracks(std::string& outstring) {
-	int code;
-
-	std::vector<std::string> merge_argv;
-	merge_argv.emplace_back("mkvmerge");
-	merge_argv.emplace_back("-o");
-	merge_argv.emplace_back(cvtctx.elem.outpath);
-
-	merge_argv.emplace_back("-a")
-	
-	std::vector<int> want_tid;
-	select_wanted_audio_tracks(want_tid);
-	assert(!want_tid.empty()); /*TODO what if we have no audio tracks? */
-	
-	std::string compact_want_audio_tid;
-	for (size_t i = 0; i+1 < want_tid.size(); i++)
-		compact_want_audio_tid += std::to_string(tid) + ",";
-	compact_want_audio_tid += std::to_string(want_tid.back());
-
-	merge_argv.emplace_back(compact_want_audio_tid);
-	merge_argv.emplace_back(cvtctx.elem.path);
-	
-	for (const std::string& producedfile : cvtctx.producedv)
-		merge_argv.emplace_back(cvtctx.elem.path);
-	
-	/*first video track, then ita audio track, then the rest */
-	std::string trackorder;
-	int videotid = util_get_first_video_track(elem)
-	if (videotid >= 0)
-		trackorder += "0:" + std::to_string(videotid);
-	/*TODO add audio track (if it's there...) */
-	merge_argv.emplace_back();
-	if (!trackorder.empty()) {
-		merge_argv.emplace_back("--track-order");
-		merge_argv.emplace_back(trackorder);
-	}
-	
-	code = launch_process(merge_argv, outstring);
-	if (code != 0)
-		return -1;
-	
-	return 0;
-}
-
-std::string mediaconvert::util_get_ac3ita_track(convert_context_t& cvtctx) {
-	for (const auto& item : cvtctx.elem.itemv) {
-		if (item.codec == "AC3") {
-			std::string lang = item.language.substr(0, 2);
-			boost::algorithm::to_lower(lang);
-			if (lang == "it")
-				return item.number; /*TODO number or UID? */
-		}
-	}
-
-	return "";
-}
-
-int mediaconvert::ac3ita_aac_convert(std::string& outstring) {
-	int code;
-	
-	std::string converter = settings::pt.get("converter", "");
-
-	std::vector<std::string> convert_argv;
-	if (converter == "ac3to") {
-		convert_argv.emplace_back("eac3to");
-		convert_argv.emplace_back(cvtctx.outfile_ac3);
-		convert_argv.emplace_back(cvtctx.outfile_aac);
-	}
-	else { /*TODO specify codec  */
-		convert_argv.emplace_back("ffmpeg");
-		convert_argv.emplace_back("-i");
-		convert_argv.emplace_back(cvtctx.outfile_ac3);
-		convert_argv.emplace_back(cvtctx.outfile_aac);
-	}
-	
-	code = launch_process(convert_argv, outstring);
-	if (code != 0)
-		return -1;
-	
-	return 0;
-}
-
-int mediaconvert::ac3ita_aac_extract(std::string& outstring) {
-	int code;
-
-	std::vector<std::string> extract_argv;
-	extract_argv.emplace_back("mkvextract");
-	extract_argv.emplace_back("--command-line-charset");
-	extract_argv.emplace_back("UTF-8");
-	extract_argv.emplace_back("--output-charset");
-	extract_argv.emplace_back("UTF-8");
-	extract_argv.emplace_back(elem.path);
-	extract_argv.emplace_back("tracks");
-	extract_argv.emplace_back(ac3ita_tid + ":" + outfile_ac3);
-	
-	code = launch_process(extract_argv, outstring);
-	if (code != 0)
-		return -1;
-	
-	return 0;
-}
-
-int mediaconvert::ac3ita_aac(std::string& outstring) {
-	int code;
-
-	code = ac3ita_aac_extract(outstring);
-	if (code < 0) /*TODO can be 1 */
-		return -1;
-	
-	code = ac3ita_aac_convert(outstring);
-	if (code != 0)
-		return -1;
-	
-	return 0;
-}
-
-void do_keep(const item_t& item, convertcontext_t& convertctx) {
-	convertcontext.keep_audio_tids.push_back(item.tid);
-}
-
-void do_convert(const media_t& elem, const item_t& item,
+int do_propedit(const media_t& elem, const item_t& item,
                 convertcontext_t& convertctx)
 {
 	
+}
+
+int do_merge(const media_t& elem, const item_t& item,
+                convertcontext_t& convertctx)
+{
+	std::vector<std::string> argv;
+	int code;
 	
-	convertctx.outsiders.emplace_back( path to out );
+	argv.push_back(app::mkvmerge_prog);
+	argv.push_back("--command-line-charset");
+#ifdef _WIN32
+	argv.push_back("UTF-16");
+#else
+	argv.push_back("UTF-8");
+#endif
+	argv.push_back("--output-charset");
+	argv.push_back("UTF-8");
+	if (!elem.title.empty()) {
+		argv.push_back("--title");
+		argv.push_back(elem.title);
+	}
+	argv.push_back("-o")
+	argv.push_back(convertctx.outname);
+	if (convertctx.want_keep_audio_tid.empty())
+		argv.push_back("-A");
+	else {
+		argv.push_back("-a");
+		argv.push_back(util_plain_comma_list(convertctx.want_keep_audio_tid));
+	}
+	argv.push_back(elem.path);
+	for (const std::string& outsider : convertctx.outsiders) {
+		argv.emplace_back("--language:ita");
+		argv.emplace_back("--default");
+		argv.emplace_back("--forced");
+		argv.emplace_back(outsider);
+	}
+	argv.push_back("--track-order");
+	/*
+	prima tutte le tracce video, poi gli audio convertiti (che sono tutti ita),
+	poi gli audio ita che sono mantenuti, quindi tutto il resto
+	*/
+	std::string track_order;
+
+	/*cerca tutte le tracce video*/
+	std::vector<size_t> all_video_tids;
+	for (const item_t& item : elem.items) {
+		if (item.type == itemtype_video) {
+			assert(item.tid >= 0);
+			all_video_tids.push_back(item.tid);
+		}
+	}
+	
+	for (size_t videotid : all_video_tids)
+		track_order += "0:" + std::to_string(videotid) + ",";
+	
+	for (size_t i = 0; i < convertctx.outsiders.size(); i++)
+		track_order += std::to_string(i + 1) + ":0,";
+	
+	for (const item_t& item : elem.items) {
+		if (item.type == itemtype_audio && item.langid == "ita") /*TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+			track_order += "0:" + std::to_string(item.tid) + ","
+	}
+	
+	/*TODO remove final , in track_order string */
+	code = launch_process(argv, outputstring);
+
+	if (code != 0)
+		return -1;
+	
+	/*TODO test existence of convertctx.outname file */
+	
+	return 0;
+}
+
+int do_keep(const item_t& item, convertcontext_t& convertctx) {
+	convertcontext.keep_audio_tids.push_back(item.tid);
+	
+	return 0;
+}
+
+int do_convert(const media_t& elem, const item_t& item,
+                convertcontext_t& convertctx)
+{
+	extractcontext_t extractctx;
+	int code;
+	
+	code = do_extract(elem, item, extractctx);
+	if (code != 0) {
+		/*TODO do_cleanup */
+		return -1;
+	}
+	
+	code = do_reencode(extractctx);
+	
+	convertctx.outsiders.emplace_back(extractctx.outpath);
+	
+	return 0;
 }
 
 bool have_to_keep(const media_t& elem, const item_t& item) {
@@ -262,13 +160,18 @@ int mediaconvert::process(size_t n, std::string& outstring) {
 		const item_t& item = elem.items[i];
 
 		if (item.type == itemtype_audio) {
-			if (have_to_convert(elem, item) {
+
+			if (have_to_convert(elem, item) {				
 				code = do_convert(elem, item, convertctx);
-				if (code != 0) return -1;
+				if (code != 0)
+					return -1;
 			}
-			else if (have_to_keep(elem, item) {
-				do_keep(elem, item, convertctx);
+			else if (have_to_keep(elem, item) {			
+				code = do_keep(elem, item, convertctx);
+				if (code != 0)
+					return -1;
 			}
+			
 		}
 	}
 	
